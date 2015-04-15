b32c035ab34ffc119de2c8d0e9fec845719344dc
David Marcos <dmarcosgon@gmail.com>
2015-04-15 18:33:29 +0200

    Added support for ddc and soft amplifier

--- drivers/iio/switches/rx1.c
+++ drivers/iio/switches/rx1.c
@@ -21,221 +21,438 @@
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
 #include <asm/io.h>
-
-struct rx1_rfio_platform_data{
-	unsigned long *base_addr;   
-	struct resource *res;      
-	unsigned long remap_size;   
+#define NUM_CHANNELS 4
+#define GAIN_STEPS 37
+const unsigned int gaincodes[37] = {
+    1024, 1149, 1289, 1446, 1623, 1821,
+    2043, 2292, 2572, 2886, 3238, 3633,
+    4077, 4574, 5132, 5758, 6461, 7249,
+    8134, 9126, 10240, 11489, 12891, 14464,
+    16229, 18210, 20431, 22925, 25722, 28860,
+    32382, 36333, 40766, 45740, 51322, 57584,
+    64610
 };
 
-struct rx1_rfio_platform_data pdata;
+struct rx1_platform_data {
+    unsigned long *base_addr;
+    struct resource *res;
+    unsigned long remap_size;
+};
 
-struct rx1_rfio_state {
-	unsigned int selected;
-	unsigned int rfen;
-	unsigned int picrst;
-	struct rx1_rfio_platform_data* pdata;
+struct rx1_platform_data pdata;
+
+struct rx1_state {
+    unsigned int selected;
+    unsigned int rfen;
+    unsigned int picrst;
+    unsigned int stage2mode;
+    unsigned int stage1mode;
+    unsigned int mixerpol;
+    unsigned int mixerbyp;
+    unsigned int ddcfrequency;
+    unsigned int ifgain;
+    unsigned int ifgaindb;
+    struct rx1_platform_data* pdata;
 };
 
 static ssize_t rx1_rfio_write(struct iio_dev *indio_dev,
-				    uintptr_t private,
-				    const struct iio_chan_spec *chan,
-				    const char *buf, size_t len)
-{
-	struct rx1_rfio_state *st = iio_priv(indio_dev);
-	unsigned long long readin;
-	int ret=0;
-	unsigned int regval=0;
-	ret = kstrtoull(buf, 10, &readin);
-	if (ret)
-		return ret;
-
-	mutex_lock(&indio_dev->mlock);
-	switch ((u32)private) {
-	case 0:
-		if(readin < 0 || readin > 4) ret = -EINVAL;
-		else st->selected = readin;
-		break;
-	case 1:
-		st->rfen = readin?0:1;	
-		break;
-	case 2:
-		st->picrst =readin?1:0;	
-		break;
-	default:
-		ret = -EINVAL;
-	}
-
-	if (!ret){
-		regval|=st->rfen;
-		regval|=st->selected?(st->selected)<<1:0xE;
-		regval|=st->picrst<<7;
-		iowrite32(regval, st->pdata->base_addr);
-	}
-	mutex_unlock(&indio_dev->mlock);
-	return ret ? ret : len;
-
+        uintptr_t private,
+        const struct iio_chan_spec *chan,
+        const char *buf, size_t len) {
+    unsigned long long readin;
+    struct rx1_state *st = iio_priv(indio_dev);
+    int ret = 0;
+    unsigned int regval = 0;
+    ret = kstrtoull(buf, 10, &readin);
+    if (ret)
+        return ret;
+    mutex_lock(&indio_dev->mlock);
+    switch ((u32) private) {
+        case 0:
+            if (readin < 0 || readin > 4) ret = -EINVAL;
+            else st->selected = readin;
+            break;
+        case 1:
+            st->rfen = readin ? 0 : 1;
+            break;
+        case 2:
+            st->picrst = readin ? 1 : 0;
+            break;
+        default:
+            ret = -EINVAL;
+    }
+    if (!ret) {
+        regval |= st->rfen;
+        regval |= st->selected ? (st->selected) << 1 : 0xE;
+        regval |= st->picrst << 7;
+        iowrite32(regval, (st->pdata->base_addr) + chan->channel);
+    }
+    mutex_unlock(&indio_dev->mlock);
+    return ret ? ret : len;
 }
 
+static ssize_t rx1_ddc_write(struct iio_dev *indio_dev,
+        uintptr_t private,
+        const struct iio_chan_spec *chan,
+        const char *buf, size_t len) {
+    unsigned long long readin;
+    struct rx1_state *st = iio_priv(indio_dev);
+    int ret = 0;
+    unsigned int regval = 0;
+    ret = kstrtoull(buf, 10, &readin);
+    if (ret)
+        return ret;
+    mutex_lock(&indio_dev->mlock);
+    switch ((u32) private) {
+        case 0:
+            if (readin < 0 || readin > 4) ret = -EINVAL;
+            else if (!st->stage1mode & 0x01) { //Bypass enabled
+                st->stage2mode = readin;
+            } else
+                st->stage2mode = 0;
+            break;
+        case 1:
+            if (readin < 0 || readin > 3)ret = -EINVAL;
+            else {
+                st->stage1mode = readin;
+                if (st->stage1mode & 0x01) { //Bypass enabled
+                    st->stage2mode = 0; //Bypass stage2
+                    st->mixerbyp = 1; //Bypass mixer
+                }
+            }
+            break;
+        case 2:
+            if (readin < 0 || readin > 1)ret = -EINVAL;
+            else st->mixerpol = readin;
+            break;
+        case 3:
+            if (readin < 0 || readin > 1)ret = -EINVAL;
+            else if (!st->stage1mode & 0x01) { //Bypass enabled
+                st->mixerbyp = readin;
+            } else {
+                st->mixerbyp = 1;
+            }
+            break;
+        default:
+            ret = -EINVAL;
+    }
+    if (!ret) {
+        regval |= st->stage2mode;
+        regval |= st->stage1mode << 4;
+        regval |= st->mixerpol << 6;
+        regval |= st->mixerbyp << 7;
+        iowrite32(regval, (st->pdata->base_addr) + chan->channel);
+    }
+    mutex_unlock(&indio_dev->mlock);
+    return ret ? ret : len;
+}
+
+static ssize_t rx1_ddcfreq_write(struct iio_dev *indio_dev,
+        uintptr_t private,
+        const struct iio_chan_spec *chan,
+        const char *buf, size_t len) {
+    unsigned long long readin;
+    struct rx1_state *st = iio_priv(indio_dev);
+    int ret = 0;
+    unsigned int regval;
+    ret = kstrtoull(buf, 10, &readin);
+    if (ret)
+        return ret;
+    mutex_lock(&indio_dev->mlock);
+    if (readin < 0 || readin > 960000)
+        ret = -EINVAL;
+    else {
+        regval = ((u32) readin * 4096) / 1875;
+        st->ddcfrequency = (regval * 1875) / 4096;
+        iowrite32(regval, (st->pdata->base_addr) + chan->channel);
+    }
+    mutex_unlock(&indio_dev->mlock);
+    return ret ? ret : len;
+}
 
 static ssize_t rx1_rfio_read(struct iio_dev *indio_dev,
-				   uintptr_t private,
-				   const struct iio_chan_spec *chan,
-				   char *buf)
-{
-struct rx1_rfio_state *st = iio_priv(indio_dev);
-	int ret=0;
-	mutex_lock(&indio_dev->mlock);
-	switch ((u32)private) {
-	case 0:
-		switch(st->selected){
-			case 0:
-				ret=sprintf(buf,"%s","Disconnected");
-				break;
-			case 1:
-				ret=sprintf(buf,"%s","Test");
-				break;
-			case 2:
-				ret=sprintf(buf,"%s","1.2GHz-3GHz");
-				break;
-			case 3:	
-				ret=sprintf(buf,"%s","50ohm load");
-				break;	
-			case 4:
-				ret=sprintf(buf,"%s","20MHz-1.2GHz");
-				break;
-			}
-		break;
-	case 1:
-		ret=sprintf(buf,"%s",st->rfen?"RF disabled":"RF enabled");	
-		break;
-	case 2:
-		ret=sprintf(buf,"%s",st->picrst?"PIC held in reset":"PIC running");	
-		break;
-	default:
-		ret = -EINVAL;
-	}
-	mutex_unlock(&indio_dev->mlock);
-	return ret;
+        uintptr_t private,
+        const struct iio_chan_spec *chan,
+        char *buf) {
+    struct rx1_state *st = iio_priv(indio_dev);
+    int ret = 0;
+    mutex_lock(&indio_dev->mlock);
+    switch ((u32) private) {
+        case 0:
+            switch (st->selected) {
+                case 0:
+                    ret = sprintf(buf, "%s", "Disconnected");
+                    break;
+                case 1:
+                    ret = sprintf(buf, "%s", "Test");
+                    break;
+                case 2:
+                    ret = sprintf(buf, "%s", "1.2GHz-3GHz");
+                    break;
+                case 3:
+                    ret = sprintf(buf, "%s", "50ohm load");
+                    break;
+                case 4:
+                    ret = sprintf(buf, "%s", "20MHz-1.2GHz");
+                    break;
+            }
+            break;
+        case 1:
+            ret = sprintf(buf, "%s", st->rfen ? "RF disabled" : "RF enabled");
+            break;
+        case 2:
+            ret = sprintf(buf, "%s", st->picrst ? "PIC held in reset" : "PIC running");
+            break;
+        default:
+            ret = -EINVAL;
+    }
+    mutex_unlock(&indio_dev->mlock);
+    return ret;
 }
 
+static ssize_t rx1_ddc_read(struct iio_dev *indio_dev,
+        uintptr_t private,
+        const struct iio_chan_spec *chan,
+        char *buf) {
+    struct rx1_state *st = iio_priv(indio_dev);
+    int ret = 0;
+    mutex_lock(&indio_dev->mlock);
+    switch ((u32) private) {
+        case 0:
+            switch (st->stage2mode) {
+                case 0:
+                    ret = sprintf(buf, "%s", "Bypass");
+                    break;
+                case 1:
+                    ret = sprintf(buf, "%s", "340kHz");
+                    break;
+                case 2:
+                    ret = sprintf(buf, "%s", "240kHz");
+                    break;
+                case 3:
+                    ret = sprintf(buf, "%s", "100kHz");
+                    break;
+                case 4:
+                    ret = sprintf(buf, "%s", "30kHz");
+                    break;
+            }
+            break;
+        case 1:
+            ret = sprintf(buf, "%s %s", st->stage1mode & 0x01 ? "Bypass" : "",
+                    st->stage1mode & 0x02 ? "12dB" : "0dB");
+            break;
+        case 2:
+            ret = sprintf(buf, "%s", st->mixerpol ? "Positive" : "Negative");
+            break;
+        case 3:
+            ret = sprintf(buf, "%s", st->mixerbyp ? "Bypass" : "Normal");
+            break;
+        default:
+            ret = -EINVAL;
+    }
+    mutex_unlock(&indio_dev->mlock);
+    return ret;
+}
+
+static ssize_t rx1_ddcfreq_read(struct iio_dev *indio_dev,
+        uintptr_t private,
+        const struct iio_chan_spec *chan,
+        char *buf) {
+    struct rx1_state *st = iio_priv(indio_dev);
+    int ret = 0;
+    mutex_lock(&indio_dev->mlock);
+    ret = sprintf(buf, "%d", st->ddcfrequency);
+    mutex_unlock(&indio_dev->mlock);
+    return ret;
+}
+
+static int rx1_ifgain_write_raw(struct iio_dev *indio_dev,
+        struct iio_chan_spec const *chan,
+        int val,
+        int val2,
+        long mask) {
+    struct rx1_state *st = iio_priv(indio_dev);
+    unsigned int code;
+    int ret;
+    if (val < 0 || val > 36 || val2 < 0)
+        return -EINVAL;
+    code = gaincodes[(u8) val]; //integer part of gain
+    mutex_lock(&indio_dev->mlock);
+    switch (mask) {
+        case IIO_CHAN_INFO_HARDWAREGAIN:
+            st-> ifgain = code;
+            st->ifgaindb = val;
+            iowrite32(st->ifgain, (st->pdata->base_addr) + chan->channel);
+            break;
+        default:
+            ret = -EINVAL;
+    }
+    mutex_unlock(&indio_dev->mlock);
+    return ret;
+}
+
+static int rx1_ifgain_read_raw(struct iio_dev *indio_dev,
+        struct iio_chan_spec const *chan,
+        int *val,
+        int *val2,
+        long m) {
+    struct rx1_state *st = iio_priv(indio_dev);
+    int ret;
+    mutex_lock(&indio_dev->mlock);
+    switch (m) {
+        case IIO_CHAN_INFO_HARDWAREGAIN:
+            *val = st->ifgaindb;
+            *val2 = 0;
+            ret = IIO_VAL_INT_PLUS_MICRO_DB;
+            break;
+        default:
+            ret = -EINVAL;
+    }
+    mutex_unlock(&indio_dev->mlock);
+
+    return ret;
+};
 
 
-#define _RX1_RFIO_EXT_INFO(_name, _ident) { \
+#define _RX1_EXT_INFO(_name, _ident, _write, _read) { \
 	.name = _name, \
-	.read = rx1_rfio_read, \
-	.write = rx1_rfio_write, \
+	.read = _read, \
+	.write = _write, \
 	.private = _ident, \
 	.shared = IIO_SEPARATE, \
 }
 
 static const struct iio_chan_spec_ext_info rx1_rfio_ext_info[] = {
-	_RX1_RFIO_EXT_INFO("branch", 0),
-	_RX1_RFIO_EXT_INFO("rfen", 1),
-	_RX1_RFIO_EXT_INFO("picrst", 2),
-	{ },
+    _RX1_EXT_INFO("branch", 0, rx1_rfio_write, rx1_rfio_read),
+    _RX1_EXT_INFO("rfen", 1, rx1_rfio_write, rx1_rfio_read),
+    _RX1_EXT_INFO("picrst", 2, rx1_rfio_write, rx1_rfio_read), {},
 };
 
-static const struct iio_chan_spec rx1_rfio_chan = {
-	.type = IIO_ALTVOLTAGE,
-	.indexed = 1,
-	.output = 1,
-	.ext_info = rx1_rfio_ext_info,
+static const struct iio_chan_spec_ext_info rx1_ddc_ext_info[] = {
+    _RX1_EXT_INFO("stage2mode", 0, rx1_ddc_write, rx1_ddc_read), //0-3
+    _RX1_EXT_INFO("stage1mode", 1, rx1_ddc_write, rx1_ddc_read), //4-5
+    _RX1_EXT_INFO("mixerpol", 2, rx1_ddc_write, rx1_ddc_read), //6
+    _RX1_EXT_INFO("mixerbyp", 3, rx1_ddc_write, rx1_ddc_read), //7
+    {},
 };
 
-static const struct iio_info rx1_rfio_info = {
-	.driver_module = THIS_MODULE,
+static const struct iio_chan_spec_ext_info rx1_ddcfreq_ext_info[] = {
+    _RX1_EXT_INFO("ddcfrequency", 0, rx1_ddcfreq_write, rx1_ddcfreq_read), {}, //24
 };
 
+static const struct iio_chan_spec rx1_chan[NUM_CHANNELS] = {
+    {.type = IIO_ALTVOLTAGE,
+        .channel = 0,
+        .indexed = 1,
+        .output = 1,
+        .ext_info = rx1_rfio_ext_info,},
+    {.type = IIO_ALTVOLTAGE,
+        .channel = 1,
+        .indexed = 1,
+        .output = 1,
+        .ext_info = rx1_ddc_ext_info,},
+    {.type = IIO_ALTVOLTAGE,
+        .channel = 2,
+        .indexed = 1,
+        .output = 1,
+        .ext_info = rx1_ddcfreq_ext_info,},
+    {.type = IIO_VOLTAGE,
+        .channel = 3,
+        .indexed = 1,
+        .output = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_HARDWAREGAIN),},
+};
 
-static int rx1_rfio_probe(struct platform_device *pdev)
-{
-	struct iio_dev *indio_dev;
-	struct rx1_rfio_state *st;
-     	int ret = 0;
- 
-     	pdata.res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-     	if (!pdata.res) {
-         	dev_err(&pdev->dev, "No memory resource\n");
-         	return -ENODEV;
-     	}
- 
-     	pdata.remap_size = pdata.res->end - pdata.res->start + 1;
-     	if (!request_mem_region(pdata.res->start, pdata.remap_size, pdev->name)) {
-         	dev_err(&pdev->dev, "Cannot request IO\n");
-         	return -ENXIO;
-    	}
- 
-     	pdata.base_addr = ioremap(pdata.res->start, pdata.remap_size);
-     	if (pdata.base_addr == NULL) {
-         	dev_err(&pdev->dev, "Couldn't ioremap memory at 0x%08lx\n",
-             		(unsigned long)pdata.res->start);
-         	ret = -ENOMEM;
-        	goto err_release_region;
-     	}
+static const struct iio_info rx1_info = {
+    .read_raw = &rx1_ifgain_read_raw,
+    .write_raw = &rx1_ifgain_write_raw,
+    .driver_module = THIS_MODULE,
+};
 
-	indio_dev = devm_iio_device_alloc(&pdev->dev, sizeof(*st));
-	if (indio_dev == NULL) {
-		ret =  -ENOMEM;
-		dev_err(&pdev->dev, "failed to allocate indio_dev\n");
-		goto err_alloc_indio;
-	}
-	st = iio_priv(indio_dev);
-	platform_set_drvdata(pdev,indio_dev);
-	st->pdata = &pdata;
+static int rx1_probe(struct platform_device *pdev) {
+    struct iio_dev *indio_dev;
+    struct rx1_state *st;
+    int ret = 0;
 
-	indio_dev->dev.parent = NULL;
-	indio_dev->name = "RX1SW";
+    pdata.res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+    if (!pdata.res) {
+        dev_err(&pdev->dev, "No memory resource\n");
+        return -ENODEV;
+    }
 
-	indio_dev->info = &rx1_rfio_info;
-	indio_dev->modes = INDIO_DIRECT_MODE;
-	indio_dev->channels = &rx1_rfio_chan;
-	indio_dev->num_channels = 1;
+    pdata.remap_size = pdata.res->end - pdata.res->start + 1;
+    if (!request_mem_region(pdata.res->start, pdata.remap_size, pdev->name)) {
+        dev_err(&pdev->dev, "Cannot request IO\n");
+        return -ENXIO;
+    }
 
-	ret = iio_device_register(indio_dev);
-	iowrite32(0x8F, pdata.base_addr);
-	udelay(1000);
-	iowrite32(0x0F, pdata.base_addr);
-     	return 0;
- 	err_alloc_indio:
-		iounmap(pdata.base_addr);
-  	err_release_region:
-     		release_mem_region(pdata.res->start, pdata.remap_size);
-     	return ret;
+    pdata.base_addr = ioremap(pdata.res->start, pdata.remap_size);
+    if (pdata.base_addr == NULL) {
+        dev_err(&pdev->dev, "Couldn't ioremap memory at 0x%08lx\n",
+                (unsigned long) pdata.res->start);
+        ret = -ENOMEM;
+        goto err_release_region;
+    }
+
+    indio_dev = devm_iio_device_alloc(&pdev->dev, sizeof (*st));
+    if (indio_dev == NULL) {
+        ret = -ENOMEM;
+        dev_err(&pdev->dev, "failed to allocate indio_dev\n");
+        goto err_alloc_indio;
+    }
+    st = iio_priv(indio_dev);
+    platform_set_drvdata(pdev, indio_dev);
+    st->pdata = &pdata;
+    indio_dev->dev.parent = NULL;
+    indio_dev->name = "RX1_CONFIG";
+    indio_dev->info = &rx1_info;
+    indio_dev->modes = INDIO_DIRECT_MODE;
+    indio_dev->channels = rx1_chan;
+    indio_dev->num_channels = NUM_CHANNELS;
+    ret = iio_device_register(indio_dev);
+    iowrite32(0x8F, pdata.base_addr);
+    udelay(1000);
+    iowrite32(0x0F, pdata.base_addr);
+    return 0;
+err_alloc_indio:
+    iounmap(pdata.base_addr);
+err_release_region:
+    release_mem_region(pdata.res->start, pdata.remap_size);
+    return ret;
 }
 
-static int rx1_rfio_remove(struct platform_device *pdev)
-{
-	struct iio_dev *indio_dev = platform_get_drvdata(pdev);
-	iio_device_unregister(indio_dev);
-     	iounmap(pdata.base_addr);
-	release_mem_region(pdata.res->start, pdata.remap_size);
-	return 0;
+static int rx1_remove(struct platform_device *pdev) {
+    struct iio_dev *indio_dev = platform_get_drvdata(pdev);
+    iio_device_unregister(indio_dev);
+    iounmap(pdata.base_addr);
+    release_mem_region(pdata.res->start, pdata.remap_size);
+    return 0;
 }
 
 #ifdef CONFIG_OF
-static const struct of_device_id rx1_rfio_dt_match[] = {
-	{ .compatible = "gr,rx1_rfio" },
-	{},
+static const struct of_device_id rx1_dt_match[] = {
+    { .compatible = "gr,rx1_rfio"},
+    {},
 };
-MODULE_DEVICE_TABLE(of, rx1_rfio_dt_match);
+MODULE_DEVICE_TABLE(of, rx1_dt_match);
 #endif
 
-static struct platform_driver rx1_rfio_driver = {
-	.driver = {
-		.name	= "rx1_rfio",
-		.owner	= THIS_MODULE,
-		.of_match_table = of_match_ptr(rx1_rfio_dt_match),
-	},
-	.probe		= rx1_rfio_probe,
-	.remove		= rx1_rfio_remove,
+static struct platform_driver rx1_driver = {
+    .driver =
+    {
+        .name = "rx1_rfio",
+        .owner = THIS_MODULE,
+        .of_match_table = of_match_ptr(rx1_dt_match),
+    },
+    .probe = rx1_probe,
+    .remove = rx1_remove,
 };
 
 
 
-module_platform_driver(rx1_rfio_driver);
+module_platform_driver(rx1_driver);
 
 MODULE_AUTHOR("David Marcos <dmarcosgon@gr.ssr.upm.es>");
-MODULE_DESCRIPTION("MACOM rx1_rfio RF SWITCH");
+MODULE_DESCRIPTION("RX1 CONFIG DRIVER ");
 MODULE_LICENSE("GPL v2");
 
